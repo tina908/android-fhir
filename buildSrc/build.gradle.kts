c1edee11e27fa075bc5b4724ec114ea4fe867ac9
Kevin Most <kevinmost@users.noreply.github.com>
2021-08-25 18:45:07 -0400

    Integrate Licensee plugin
    Will fail our build if non-compliant licenses are added.
    
    Currently allowlisting any dependencies that are non-compliant, so we
    can get this plugin enabled. Then we can go back in follow-ups and
    decide whether those dependencies need to be removed.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,8 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
 
-plugins { `kotlin-dsl` }
+plugins {
+  `kotlin-dsl`
+}
 
 repositories {
   google()
@@ -9,5 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.12.5")
+  // Necessary. See: https://youtrack.jetbrains.com/issue/KT-31643#focus=Comments-27-4818475.0-0
+  implementation("com.android.tools.build:gradle:4.2.2")
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.5.30")
+
+  implementation("com.diffplug.spotless:spotless-plugin-gradle:5.12.5")
+  implementation("app.cash.licensee:licensee-gradle-plugin:1.2.0")
 }
